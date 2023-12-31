8db1c0ca039586602fb8f434d10188f95290aa2d
sagivmaoz@gmail.com <sagivmaoz@gmail.com>
2023-12-31 19:43:46 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program0257.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Specialized;
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome0257();
+        Welcome4079();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome4079();
+    private static void Welcome0257()
+    {
+        Console.WriteLine("Enter your name: ");
+        string theName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", theName);
+    }
+}
