075e34ee29529f8788dbe9829021a146512cc642
eitan weizman <eitan9611@gmail.com>
2023-12-31 20:12:28 +0200

    Partner 2 takes part in the partial class

--- Stage0/Program0257.cs
+++ Stage0/Program0257.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections.Specialized;
 
-partial class Program
-{
-    private static void Main(string[] args)
-    {
-        Welcome0257();
-        Welcome4079();
-        Console.ReadKey();
-    }
 
-    static partial void Welcome4079();
-    private static void Welcome0257()
+namespace Stage0 {
+    partial class Program
     {
-        Console.WriteLine("Enter your name: ");
-        string theName = Console.ReadLine();
-        Console.WriteLine("{0}, welcome to my first console application", theName);
+        private static void Main(string[] args)
+        {
+            Welcome0257();
+            Welcome4079();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome4079();
+        private static void Welcome0257()
+        {
+            Console.WriteLine("Enter your name: ");
+            string theName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", theName);
+        }
     }
 }
