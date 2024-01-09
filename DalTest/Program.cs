713dd134d785dbcede0f47016248ccf29d467a0c
eitan weizman <eitan9611@gmail.com>
2024-01-09 20:52:41 +0200

    beginning of part 9 (together)

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Dal;
 using DalApi;
+using System.Reflection.Metadata;
 
 namespace DalTest
 {
@@ -13,21 +14,33 @@ namespace DalTest
             try
             {
                 Initialization.Do(s_dalTask!, s_dalEngineer!, s_dalDependency!);
-
-
-
-
-
-
-
-
-
-
+                int choice=1;
+                while(!(choice==0))
+                {
+                    Console.WriteLine("Select an entity you want to check:\n 0. exit from the menu.\n1. Engineer.\n2. Dependency.\n3.Task");
+                    choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:EntityMenu("Engineer");
+                            break;
+                        case 2:EntityMenu("Dependency");
+                            break;
+                        case 3:EntityMenu("Task");
+                            break;
+                        case 0: break;
+                        default: break;
+                    }
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(  );
             }
         }
+        public static void EntityMenu(string typeChoice)
+        {
+
+        }
+
     }
 }
