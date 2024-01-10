99bff01f4a8b05a739fbc23014fee3b8c1fbc83f
eitan weizman <eitan9611@gmail.com>
2024-01-10 19:26:54 +0200

    document the whole project without program.cs

--- DalList/EngineerImplementation.cs
+++ DalList/EngineerImplementation.cs
@@ -5,12 +5,18 @@ using System.Collections.Generic;
 
 public class EngineerImplementation : IEngineer
 {
+    /// <summary>
+    /// this method gets a new engineer and if he doesnt exist in the engineer list- add him. else throw.
+    /// </summary>
+    /// <param name="item"> the new engineer we want to add</param>
+    /// <returns>the id of the new engineer</returns>
+    /// <exception cref="Exception"></exception>
     public int Create(Engineer item)
     {
         Engineer? ifExists = DataSource.Config.FindEngineer(item._id);
         if (ifExists!= null)
         {
-            if (ifExists._isActive == false)
+            if (ifExists._isActive == false)//if we didnt found him and its ok to add him 
             {
                 Engineer updatedEngineer = item with { _isActive = true };
                 int theIndex = DataSource.Config.FindIndexEngineer(ifExists._id);
@@ -18,7 +24,7 @@ public class EngineerImplementation : IEngineer
                 DataSource.Engineers.Add(updatedEngineer);
                 return item._id;
             }
-            if(ifExists._isActive ==true ) 
+            if(ifExists._isActive ==true ) // if we found him and we cant add him
             {
                 throw new Exception($"Engineer with ID={ifExists._id} already exists");
             }
@@ -28,13 +34,17 @@ public class EngineerImplementation : IEngineer
         DataSource.Engineers.Add(newEngineer);
         return item._id;
     }
-
+    /// <summary>
+    /// this method set an engineer to not active if he exists(we can know his existence by given id), else throw.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <exception cref="Exception"></exception>
     public void Delete(int id)
     {
         Engineer? oldEngineer = DataSource.Config.FindEngineer(id);
-        if (oldEngineer != null && oldEngineer._isActive==true)
+        if (oldEngineer != null && oldEngineer._isActive==true) //if the given id is in the list and he active 
         {
-            Engineer updatedEngineer = oldEngineer! with { _isActive = false};
+            Engineer updatedEngineer = oldEngineer! with { _isActive = false};//create new one with the same details but not active.
             int theIndex=DataSource.Config.FindIndexEngineer(id);
             DataSource.Engineers.RemoveAt(theIndex);
             DataSource.Engineers.Add(updatedEngineer);
@@ -42,11 +52,15 @@ public class EngineerImplementation : IEngineer
         }
         throw new Exception($"Engineer with ID={id} does Not exist");
     }
-
+    /// <summary>
+    /// this method gets id and returns the engineer in the list that has this id, if not exists so return null.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public Engineer? Read(int id)
     {
         Engineer? returnedValue = DataSource.Config.FindEngineer(id);
-        if(returnedValue != null && returnedValue._isActive == true)
+        if(returnedValue != null && returnedValue._isActive == true)//if the id is exist in the list and the founded engineer is active.
         {
             return returnedValue;
         }
@@ -56,7 +70,10 @@ public class EngineerImplementation : IEngineer
         }
 
     }
-
+    /// <summary>
+    /// this method creats a new list of engineers with the same details of the old list.
+    /// </summary>
+    /// <returns>the new list</returns>
     public List<Engineer?> ReadAll()
     {
         Engineer? temp;
@@ -68,11 +85,15 @@ public class EngineerImplementation : IEngineer
         }
         return newList;
     }
-
+    /// <summary>
+    /// this program gets a new engineer and removes the old engineer with the same id that in the list and gets the new one in. 
+    /// </summary>
+    /// <param name="item">the new engineer </param>
+    /// <exception cref="Exception"></exception>
     public void Update(Engineer item)
     {
         Engineer? oldValue = DataSource.Config.FindEngineer(item._id);
-        if(oldValue==null || oldValue._isActive==false)
+        if(oldValue==null || oldValue._isActive==false) //if the engineer from the list isn't active or not found 
         {
             throw new Exception($"Engineer with ID={item._id} does Not exist");
         }
