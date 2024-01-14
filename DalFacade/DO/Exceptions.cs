28185dd7a4a671a89cb57ceba19105c2f3d8a384
eitan weizman <eitan9611@gmail.com>
2024-01-14 20:55:54 +0200

    Exceptions class

---
+++ DalFacade/DO/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DO;
+[Serializable]
+public class DalNotFoundException : Exception
+{
+    DalNotFoundException(string? message) : base(message) { }
+}
+public class DalAlreadyExistsException : Exception
+{
+    DalAlreadyExistsException(string? message) : base(message) { }
+}
