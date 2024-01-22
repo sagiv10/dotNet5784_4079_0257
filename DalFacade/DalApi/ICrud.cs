cfb9c6d8c8b87f7181a05d44db067519a62e3be1
sagivmaoz@gmail.com <sagivmaoz@gmail.com>
2024-01-23 00:24:15 +0200

    not relevent

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -18,5 +18,7 @@ namespace DalApi
         IEnumerable<T?> ReadAll(Func<T, bool>? filter = null); //stage 1 only, Reads all entity objects
         void Update(T item); //Updates entity object
         void Delete(int id); //Deletes an object by its Id
+
+        void DeleteAll(); //
     }
 }
