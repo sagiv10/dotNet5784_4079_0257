5cf26ede5d35e7b4b50eb31644be44fbb56fbdfc
sagivmaoz@gmail.com <sagivmaoz@gmail.com>
2024-01-28 21:39:11 +0200

    check

--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -7,7 +7,7 @@ sealed internal class DalXml : IDal //helping singleton entity to get access to 
 {
     private static Lazy<IDal> instance = new Lazy<IDal>(() => new DalXml(), LazyThreadSafetyMode.ExecutionAndPublication);
     //field of the lazy singleton so that he will be safe threding - when different cores will try create different instances at once, it will create only one instance
-    public static IDal Instance => instance.Value; //returns the entity by the rules of lazy singleton
+    public static IDal Instance { get => instance.Value;} //returns the entity by the rules of lazy singleton
     // if the 'LazyThreadSafetyMode.ExecutionAndPublication' indicate that there isn't instane exist then use the constructor, gaven to the Lazy<IDal> to create new instance and return it, else- return the existing instance
     private DalXml() { }
     public ITask Task => new TaskImplementation();
