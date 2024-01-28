b3688ac62531a7dbf3d78837413180e5d5cca72b
eitan weizman <eitan9611@gmail.com>
2024-01-28 21:24:19 +0200

    lazy and safety dallist

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -7,8 +7,20 @@ using System.Threading.Tasks;
 
 namespace Dal
 {
-    sealed public class DalList : IDal
+    sealed internal class DalList : IDal
     {
+        //SINGLETON CLASS:-----------------------------------
+        private static readonly Lazy<IDal> instance =
+        new Lazy<IDal>(() => new DalList(), LazyThreadSafetyMode.PublicationOnly);
+        //the lazy installation is waiting for the first time we will actually use the instance. 
+        //how? the normal singleton class directly create variable when we start the program. but here
+        //the property not creating the variable himself.it creats a delegate that
+        //will create the variable only in the first time we will actually use the varibale, using
+        //the last line [with the (.Value), if you see what .Value do you will see that Lazy class check if the
+        //instance exist, if not-so create. and that the first create]
+        private DalList() { }
+        public static IDal Instance => instance.Value;
+        //---------------------------------------------------
         public ITask Task => new TaskImplementation();
 
         public IEngineer Engineer => new EngineerImplementation();
