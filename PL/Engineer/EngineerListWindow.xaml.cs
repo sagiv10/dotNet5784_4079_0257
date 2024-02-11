906dfb0237687553323769640f05ca075bc5332e
eitan weizman <eitan9611@gmail.com>
2024-02-11 22:32:24 +0200

    end of part 6

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -19,9 +19,23 @@ namespace PL.Engineer
     /// </summary>
     public partial class EngineerListWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        public IEnumerable<BO.Engineer> EngineerList
+        {
+            get { return (IEnumerable<BO.Engineer>)GetValue(EngineerListProperty); }
+            set { SetValue(EngineerListProperty, value); }
+        }
+
+        public static readonly DependencyProperty EngineerListProperty = 
+            DependencyProperty.Register("EngineerList", typeof(IEnumerable<BO.Engineer>), typeof(EngineerListWindow), new PropertyMetadata(null));
+
+
         public EngineerListWindow()
         {
+
             InitializeComponent();
+            EngineerList = s_bl?.Engineer.ReadAllEngineers()!;
+
         }
     }
 }
