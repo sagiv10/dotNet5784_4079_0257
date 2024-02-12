47e108d09f91a0133d41a7530b095c344e4ae2ab
sagivmaoz@gmail.com <sagivmaoz@gmail.com>
2024-02-12 22:58:28 +0200

    part 5 stage 5

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using PL.Engineer;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +12,7 @@ using System.Windows.Shapes;
 
 namespace PL
 {
+    using DalTest;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -19,6 +21,21 @@ namespace PL
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void InitializeButton(object sender, RoutedEventArgs e)
+        {            
+            if(MessageBox.Show("are you sure you want to initialize the data base?", //the messege
+                "Initialization confirm", //title
+                MessageBoxButton.YesNo) == MessageBoxResult.Yes) //if the user answered 'yes'
+            {
+                DalTest.Initialization.Do();
+            }
+        }
+
+        private void HandleEngineersButton(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
         }
     }
 }
